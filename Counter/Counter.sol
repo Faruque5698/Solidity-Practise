9b8ad627bbad0b4ff587f63c745d9879cf5353b1
Md. Ashaduzzaman Faruque <Ashaduzzaman5698@gmail.com>
2025-09-16 11:02:52 +0600

    Add files via upload
    simplefy

--- Counter/Counter.sol
+++ Counter/Counter.sol
@@ -4,16 +4,22 @@ pragma solidity ^0.8.0;
 
 contract Counter {
     // Code like class...
-    uint count; //1,2,3,4
+    // uint count; //1,2,3,4
+
+    uint public count = 0;
+     //uint = data type
+     //count = variable name
+     //public = visibility
+     //0 = initial value
 
 
-    constructor(){
-        count = 0;
-    }
+    // constructor(){
+    //     count = 0;
+    // }
 
-    function getCount() public view returns(uint) {
-        return count;
-    }
+    // function getCount() public view returns(uint) {
+    //     return count;
+    // }
 
     //getCount = function name  
      //public = visibility
